1a547e340cefcb8552d60a17487cc3880cc2c3f6
Argie Barcena <barcenaargie@gmail.com>
2022-03-10 01:16:05 +0800

    fix rubocop errors

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -16,6 +16,7 @@ class PortfoliosController < ApplicationController
   end
 
   private
+
   def portfolio_params
     params.require(:portfolio).permit(:unit, :market_id, :market_symbol, :transaction_type, :revenue)
   end
@@ -30,38 +31,38 @@ class PortfoliosController < ApplicationController
 
   def portfolio_buy_logic(is_market_available)
     if is_market_available.nil?
-        @portfolio = current_trader.Portfolios.build(portfolio_params)
-        @portfolio.revenue = params[:portfolio][:revenue].to_f.round(2) * params[:portfolio][:unit].to_f.round(2)
-        @trader_wallet.balance = current_trader.wallet.balance - params[:portfolio][:revenue].to_f.round(2) * params[:portfolio][:unit].to_f.round(2)
-        @portfolio.save if @trader_wallet.balance.positive?
-        portfolio_buy_logic_update(@trader_wallet, @portfolio)
+      @portfolio = current_trader.Portfolios.build(portfolio_params)
+      @portfolio.revenue = params[:portfolio][:revenue].to_f.round(2) * params[:portfolio][:unit].to_f.round(2)
+      @trader_wallet.balance = current_trader.wallet.balance - (params[:portfolio][:revenue].to_f.round(2) * params[:portfolio][:unit].to_f.round(2))
+      @portfolio.save if @trader_wallet.balance.positive?
+      portfolio_buy_logic_update(@trader_wallet, @portfolio)
     else
-        @portfolio = is_market_available
-        @portfolio.unit += params[:portfolio][:unit].to_i
-        @portfolio.revenue = @portfolio.revenue.to_f.round(2) + (params[:portfolio][:revenue].to_f.round(2) * params[:portfolio][:unit].to_f.round(2)).to_f.round(2)
-        @trader_wallet.balance = current_trader.wallet.balance - params[:portfolio][:revenue].to_f.round(2) * params[:portfolio][:unit].to_f.round(2)
-        @portfolio.save if @trader_wallet.balance.positive?
-        portfolio_buy_logic_create(@trader_wallet, @portfolio)
+      @portfolio = is_market_available
+      @portfolio.unit += params[:portfolio][:unit].to_i
+      @portfolio.revenue = @portfolio.revenue.to_f.round(2) + (params[:portfolio][:revenue].to_f.round(2) * params[:portfolio][:unit].to_f.round(2)).to_f.round(2)
+      @trader_wallet.balance = current_trader.wallet.balance - (params[:portfolio][:revenue].to_f.round(2) * params[:portfolio][:unit].to_f.round(2))
+      @portfolio.save if @trader_wallet.balance.positive?
+      portfolio_buy_logic_create(@trader_wallet, @portfolio)
     end
   end
 
   def portfolio_buy_logic_update(trader_wallet, portfolio)
-      if trader_wallet.balance.positive?
-        if portfolio.save
-          trader_wallet.save        
-          redirect_to trader_portfolio_path, notice: 'Successfully bought stocks'
-        else
-          render :new
-        end
+    if trader_wallet.balance.positive?
+      if portfolio.save
+        trader_wallet.save
+        redirect_to trader_portfolio_path, notice: 'Successfully bought stocks'
       else
-        redirect_back fallback_location: :trader_portfolio_path, notice: 'Please check if balance is sufficient.'
+        render :new
       end
+    else
+      redirect_back fallback_location: :trader_portfolio_path, notice: 'Please check if balance is sufficient.'
+    end
   end
 
   def portfolio_buy_logic_create(trader_wallet, portfolio)
     if trader_wallet.balance.positive?
       if portfolio.save
-        trader_wallet.save        
+        trader_wallet.save
         redirect_to trader_portfolio_path, notice: 'Successfully bought stocks'
       else
         render :new
